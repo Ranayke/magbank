62aa49fbec4c0b0c93126f80cae242e9c6ddc1c6
Ranayke Boni Ferreira <ranayke2009@gmail.com>
2022-11-08 07:16:21 -0300

    feat(dashboard): implemented dashboard page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Navbar, Nav, ButtonGroup, Button, NavDropdown } from "react-bootstrap";
 import "./Navbar.scss";
 import logo from "../assets/logo.svg";
@@ -7,12 +8,14 @@ const Navigation = ({ handleCreateAcc }) => (
   <Navbar variant='dark' expand='lg'>
     <Container>
       <Navbar.Brand href='#home'>
+        <Link to="/">
         <img
           src={logo}
           height='30'
           className='d-inline-block align-top'
           alt='Magbank logo'
         />
+        </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls='basic-navbar-nav' />
       <Navbar.Collapse id='basic-navbar-nav'>
@@ -25,11 +28,15 @@ const Navigation = ({ handleCreateAcc }) => (
           <Button variant='outline-light'>
             <NavDropdown title='Acessar minha conta' id='basic-nav-dropdown'>
               <NavDropdown.Item href='#action/3.1'>
+                <Link to='/login'>
                 Pessoa Física
+                </Link>
               </NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href='#action/3.2'>
+                <Link to='/login'>
                 Pessoa Jurídica
+                </Link>
               </NavDropdown.Item>
             </NavDropdown>
           </Button>
